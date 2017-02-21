fa1354aea8a4f961eaf8076d81077c5be0261b6c
locohuang <hpquan@126.com>
2017-02-21 08:46:30 +0800

    my

--- src/HelloJava.java
+++ src/HelloJava.java
@@ -2,6 +2,7 @@
  * Created by Administrator on 2017-2-21.
  */
 public class HelloJava {
+    private int age;
     public static void main(String[] args) {
         System.out.println("My First Java Program!");
     }
